807b98cf09a035f84502c545e016eb0456b5e317
IchimVladAndrei <115074815+IchimVladAndrei@users.noreply.github.com>
2022-11-27 20:42:06 +0200

    Update

--- Tema2/second_solution/var2.c
+++ Tema2/second_solution/var2.c
@@ -15,20 +15,35 @@ int input_arr(int n, int arr[]) {
 
 	}
 }
-int sum(int n, int arr[]) {
-	int s = 0;
-	for (int i = 1; i <= n; i++)
-		s += arr[i];
+int maxi(int n, int arr[]) {
+	int s = arr[1];
+	for (int i = 2; i <= n; i++)
+		if (arr[i] >= s)
+			s = arr[i];
 	return s;
 
 }
-void out(int sum_integers) {
-	printf("The sum of integers is: \n");
-	printf("%d", sum_integers);
+int mini(int n, int arr[]) {
+	int s = arr[1];
+	for (int i = 2; i <= n; i++)
+		if (arr[i] <= s)
+			s = arr[i];
+	return s;
+
+}
+void out(int nr) {
+	printf("The maximum number is: \n");
+	printf("%d\n", nr);
+}
+void out1(int nr) {
+	printf("The minimum number is: \n");
+	printf("%d\n", nr);
 }
 int main() {
 	int nr_integers, arr[101];
 	nr_integers = input();
 	input_arr(nr_integers, arr);
-	out(sum(nr_integers, arr));
+	out(maxi(nr_integers, arr));
+	out1(mini(nr_integers, arr));
+
 }
