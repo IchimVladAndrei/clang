e6dcad3da2535cc5a06ea928d9d76c56793c4afd
IchimVladAndrei <115074815+IchimVladAndrei@users.noreply.github.com>
2022-11-27 20:41:40 +0200

    Update

--- Tema2/first_solution/start.c
+++ Tema2/first_solution/start.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 int main() {
-	int n, a[101], s = 0;
+	int n, a[101], s = 0, mini, maxi;
 	printf("Enter how many items you have to add:\n");
 	scanf_s("%d", &n);
-	for (int i = 1; i <= n; i++){
-	scanf_s("%d", &a[i]);
+	for (int i = 1; i <= n; i++) {
+		printf(">");
+		scanf_s("%d", &a[i]);
+	}
+	mini = maxi = a[1];
+	for (int i = 1; i <= n; i++){		
+		if (a[i] >= maxi)
+			maxi = a[i];
+	
+		if (a[i] <= mini)
+			mini = a[i];
 }
-	for (int i = 1; i <= n; i++)
-		s += a[i];
-	printf("\nTheir sum is: % d", s);
+	printf("\nThe maximum number is:\n%d", maxi);
+	printf("\nThe minimum number is:\n%d", mini);
+
 }
